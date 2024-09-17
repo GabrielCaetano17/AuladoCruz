ec71f46720a9431d58b705a01bf85c4e22da58fb
Gabriel Caetano <biellcaetano11@gmail.com>
2024-09-17 08:31:56 -0300

    Atualização

--- KitFit/src/main/java/br/itb/projeto/KitFit/rest/controller/MensalidadeController.java
+++ KitFit/src/main/java/br/itb/projeto/KitFit/rest/controller/MensalidadeController.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.itb.projeto.KitFit.model.entity.Mensalidade;
-import br.itb.projeto.KitFit.model.repository.MensalidadeRepository;
-import br.itb.projeto.KitFit.service.MensagemService;
 import br.itb.projeto.KitFit.service.MensalidadeService;
 
 @RestController
